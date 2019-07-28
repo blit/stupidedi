1aa14e0b76c6c195f70f6a2cf11dafbff7ec9821
Jeremy Fujimoto-Johnson <jeremy@blit.com>
2019-07-28 08:31:01 -0700

    Remove untested 7010 guides

--- lib/stupidedi/contrib/007010.rb
+++ lib/stupidedi/contrib/007010.rb
@@ -3,9 +3,6 @@ module Stupidedi
   module Contrib
     module SeventyTen
 
-      autoload :Guides,
-        "stupidedi/contrib/007010/guides"
-
       autoload :TransactionSetDefs,
         "stupidedi/contrib/007010/transaction_set_defs"
 
